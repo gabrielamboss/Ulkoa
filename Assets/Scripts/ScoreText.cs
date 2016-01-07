ea275267c3a3471310fc7878af3b71b5d6573e9e
Gabriel Amboss <gabriel.amboss@gmail.com>
2016-01-06 23:24:29 -0200

    Adicionei porcentagem na endscene

--- Assets/Scripts/ScoreText.cs
+++ Assets/Scripts/ScoreText.cs
@@ -5,15 +5,18 @@ using System.Collections;
 public class ScoreText : MonoBehaviour {
 
 	public static int scoreCorrect, scoreWrong;
-	public Text textCorrect, textWrong;
+	private float percentScore;
+	public Text textCorrect, textWrong, textPercent;
 
 
 	// Use this for initialization
 	void Start () {
 		scoreCorrect = GlobalVariables.correctAnswerAmount;
 		scoreWrong = GlobalVariables.wrongAnswerAmount;
+		percentScore = ((float)scoreCorrect/((float)scoreCorrect+(float)scoreWrong));
 		textCorrect.text = scoreCorrect.ToString();
 		textWrong.text = scoreWrong.ToString();
+		textPercent.text = (percentScore*100).ToString("n2")+"%";
 	}
 	
 	// Update is called once per frame
