8531864613a9dcac28ebb632f759f4d6bce09c20
Bruno Henrique Machado <brunohenrique.t17@gmail.com>
2016-01-07 20:32:28 -0200

    Scripts de fade entre as cenas

---
+++ Assets/Scripts/DisplayUser.cs
@@ -0,0 +1,20 @@
+﻿using Parse;
+using UnityEngine.UI;
+using UnityEngine;
+using System.Collections;
+
+public class DisplayUser : MonoBehaviour
+{
+    public Text userLabel;
+
+    // Use this for initialization
+    void Start()
+    {
+        userLabel.text = "Logged in as " + ParseUser.CurrentUser.Username;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
