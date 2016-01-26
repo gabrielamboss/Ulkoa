5ed2d9daf4c315e7903069715c2049641a050d57
LuizFelipeh <luizfelipeh95@gmail.com>
2016-01-26 15:18:14 -0200

    End Game Scene atualizada

---
+++ Assets/CurrencyGiver.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using UnityEngine.UI;
+
+public class CurrencyGiver : MonoBehaviour {
+    public Button button;
+
+    // Use this for initialization
+    void Start() {
+        if (GlobalVariables.wrongAnswerAmount == 0)
+        {
+            ShowCurrencyGain();
+            button.enabled = false;        }
+	
+	}
+
+    private void ShowCurrencyGain()
+    {
+        gameObject.transform.position = new Vector3(400, 330, 0);
+    }
+
+    // Update is called once per frame
+    public void HidePopUp()
+    {
+        button.enabled = true;
+        Destroy(gameObject);
+    }
+}
