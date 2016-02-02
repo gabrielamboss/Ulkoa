25e571240c25f63e8438caf0bf5a6a9da94063fa
Gabriel Amboss <gabriel.amboss@gmail.com>
2016-02-02 17:48:06 -0200

    Hotfix de animações que não apareciam: Transformei animations em animators

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -170,8 +170,7 @@ public class GameManager : MonoBehaviour
         {
             GlobalVariables.wrongAnswerAmount++;
             if (currentCard.LeitnerLevel > 1) currentCard.LeitnerLevel--;
-            Debug.Log("Animation " + WrongSymbol.GetComponent<Animation>().Play());
-            WrongSymbol.GetComponent<Animation>().Play();
+			WrongSymbol.GetComponent<Animator>().Play("Playing");
             DestroyImmediate(cardObject);
             exitWrongCard(cardObject);
             Debug.Log("Voce quase acertou");
@@ -182,8 +181,7 @@ public class GameManager : MonoBehaviour
     {
         GlobalVariables.correctAnswerAmount++;
         if (currentCard.LeitnerLevel < 5) currentCard.LeitnerLevel++;
-        Debug.Log("Animation " + CorrectSymbol.GetComponent<Animation>().Play());
-        CorrectSymbol.GetComponent<Animation>().Play();
+        CorrectSymbol.GetComponent<Animator>().Play("Playing");
         DestroyImmediate(cardObject);
         exitCorrectCard(cardObject);
         Debug.Log("Voce acertou");
@@ -195,8 +193,7 @@ public class GameManager : MonoBehaviour
         {
             GlobalVariables.wrongAnswerAmount++;
             currentCard.LeitnerLevel = 1;
-            WrongSymbol.GetComponent<Animation>().Play();
-            Debug.Log("Animation " + WrongSymbol.GetComponent<Animation>().Play());
+            WrongSymbol.GetComponent<Animator>().Play("Playing");
             DestroyImmediate(cardObject);
             exitWrongCard(cardObject);
             Debug.Log("Voce errou");
